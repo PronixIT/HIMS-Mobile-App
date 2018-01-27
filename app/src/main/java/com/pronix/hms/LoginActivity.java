ea69760c7b526a72f2ee0530ee4b39fe50e36d5a
craviteja <ravi@pronixits.com>
2018-01-27 01:47:51 -0500

    Dashboard new Screen

--- app/src/main/java/com/pronix/hms/LoginActivity.java
+++ app/src/main/java/com/pronix/hms/LoginActivity.java
@@ -1,11 +1,23 @@
 package com.pronix.hms;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Typeface;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.annotation.Nullable;
+import android.support.annotation.RequiresApi;
 import android.support.v7.widget.CardView;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 import it.sephiroth.android.library.easing.Back;
 import it.sephiroth.android.library.easing.EasingManager;
@@ -14,15 +26,75 @@ import it.sephiroth.android.library.easing.EasingManager;
  * Created by ravi on 1/25/2018.
  */
 
-public class LoginActivity extends Activity{
+public class LoginActivity extends Activity implements View.OnClickListener{
     CardView singupButton = null;
+    TextView forgetPassword, tv_HeaderLogin;
+    EditText et_User, et_Password;
+    Button but_Login;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.content_main);
         singupButton = (CardView) findViewById(R.id.singup_button);
-        singupButton.setVisibility(View.VISIBLE);
+        singupButton.setOnClickListener(this);
+        singupButton.setVisibility(View.GONE);
+        initializeControls();
+        setTypeface();
         showSingupButton();
+    }
+    public void initializeControls()
+    {
+        et_User = (EditText) findViewById(R.id.et_User);
+        et_Password = (EditText) findViewById(R.id.et_Password);
+        tv_HeaderLogin = (TextView) findViewById(R.id.tv_LOGIN);
+        but_Login = (Button) findViewById(R.id.but_Login);
+
+        et_User.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                et_User.setTextSize(28);
+                et_User.setElevation(20);
+            }
+
+            @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+            @Override
+            public void afterTextChanged(Editable editable) {
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        et_User.setTextSize(24);
+                        et_User.setElevation(0);
+                    }
+                },200);
+
+            }
+        });
+    }
+
+    public void setTypeface()
+    {
+        Typeface custom_font = Typeface.createFromAsset(getAssets(),  "fonts/opensansitalic.ttf");
+        forgetPassword = (TextView) findViewById(R.id.forgetPassword);
+        forgetPassword.setTypeface(custom_font);
+
+        Typeface customFont = Typeface.createFromAsset(getAssets(),  "fonts/opensansregular.ttf");
+        et_User.setTypeface(customFont);
+
+        Typeface customopensanslight = Typeface.createFromAsset(getAssets(),  "fonts/opensanslight.ttf");
+        et_Password.setTypeface(customopensanslight);
+        Typeface lobster = Typeface.createFromAsset(getAssets(),  "fonts/lobster.otf");
+        tv_HeaderLogin.setTypeface(lobster);
+
+        Typeface customroboto = Typeface.createFromAsset(getAssets(),  "fonts/robotoregular.ttf");
+        but_Login.setTypeface(customroboto);
+
+
     }
 
     private void showSingupButton() {
@@ -71,6 +143,18 @@ public class LoginActivity extends Activity{
             }
         });
 
-        manager.start(Back.class, EasingManager.EaseType.EaseInOut-, 0, 1, 2600);
+        manager.start(Back.class, EasingManager.EaseType.EaseInOut, 0, 1, 1600);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId())
+        {
+            case R.id.singup_button:
+                Intent in = new Intent();
+                in.setClass(this, RegistrationActivity.class);
+                startActivity(in);
+                break;
+        }
     }
 }
