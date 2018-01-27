76d79d68e57b7f685ee58439b827754a101cd97f
craviteja <ravi@pronixits.com>
2018-01-27 01:53:38 -0500

    Dashboard new Screen

---
+++ app/src/main/java/com/pronix/hms/RegistrationActivity.java
@@ -0,0 +1,17 @@
+package com.pronix.hms;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by ravi on 1/26/2018.
+ */
+
+public class RegistrationActivity extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration);
+    }
+}
